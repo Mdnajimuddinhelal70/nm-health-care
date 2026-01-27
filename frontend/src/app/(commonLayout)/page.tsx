6574490e886e495bf2b641872c90184657590c78
unknown <najimuddinhelalal670@gmail.com>
2026-01-27 11:13:07 +0600

    added Steps section

--- frontend/src/app/(commonLayout)/page.tsx
+++ frontend/src/app/(commonLayout)/page.tsx
@@ -1,11 +1,13 @@
 import { Hero } from "@/src/components/modules/Home/Hero";
 import Specialities from "@/src/components/modules/Home/Specialties";
+import Steps from "@/src/components/modules/Home/Steps";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Specialities />
+      <Steps />
     </>
   );
 }
