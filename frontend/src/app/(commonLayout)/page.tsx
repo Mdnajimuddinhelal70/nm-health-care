f7bcedaae94314784bed1dd80dfd5560a334d567
unknown <najimuddinhelalal670@gmail.com>
2026-01-27 11:26:18 +0600

    added testimpnials and top related doctos sections

--- frontend/src/app/(commonLayout)/page.tsx
+++ frontend/src/app/(commonLayout)/page.tsx
@@ -1,13 +1,17 @@
 import { Hero } from "@/src/components/modules/Home/Hero";
 import Specialities from "@/src/components/modules/Home/Specialties";
 import Steps from "@/src/components/modules/Home/Steps";
+import Testimonials from "@/src/components/modules/Home/Testimonials";
+import TopRatedDoctors from "@/src/components/modules/Home/TopRatedDoctors";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Specialities />
+      <TopRatedDoctors />
       <Steps />
+      <Testimonials />
     </>
   );
 }
