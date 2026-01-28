a76cb879a1d3c68cee4f5c8ff88dd41366a874da
unknown <najimuddinhelalal670@gmail.com>
2026-01-28 23:58:52 +0600

    added register form ui

--- frontend/src/app/(commonLayout)/page.tsx
+++ frontend/src/app/(commonLayout)/page.tsx
@@ -3,15 +3,27 @@ import Specialities from "@/src/components/modules/Home/Specialties";
 import Steps from "@/src/components/modules/Home/Steps";
 import Testimonials from "@/src/components/modules/Home/Testimonials";
 import TopRatedDoctors from "@/src/components/modules/Home/TopRatedDoctors";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
-      <Hero />
-      <Specialities />
-      <TopRatedDoctors />
-      <Steps />
-      <Testimonials />
+      <Head>
+        <title>AI-Powered Healthcare - Find Your Perfect Doctor</title>
+        <meta
+          name="description"
+          content="Discover top-rated doctors tailored to your needs with our AI-powered healthcare platform. Get personalized recommendations and book appointments effortlessly."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <Hero />
+        <Specialities />
+        <TopRatedDoctors />
+        <Steps />
+        <Testimonials />
+      </main>
     </>
   );
 }
